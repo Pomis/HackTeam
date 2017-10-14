4389a879b9a5cfb32929109df35c3cecafaef437
djavid <dpkhalilov@edu.hse.ru>
2017-10-14 02:05:25 +0200

    candidates activity

---
+++ app/src/main/java/kekify/io/hackteam/activities/Candidates.java
@@ -0,0 +1,15 @@
+package kekify.io.hackteam.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kekify.io.hackteam.R;
+
+public class Candidates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candidates);
+    }
+}
