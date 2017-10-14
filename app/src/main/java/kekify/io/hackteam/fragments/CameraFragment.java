8bd8a36edf659ef96bfc1863fb06db51fa1e8ebb
Pomis <pomis172@gmail.com>
2017-10-14 02:06:36 +0200

    init

--- app/src/main/java/kekify/io/hackteam/fragments/CameraFragment.java
+++ app/src/main/java/kekify/io/hackteam/fragments/CameraFragment.java
@@ -1,12 +1,21 @@
 package kekify.io.hackteam.fragments;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.CameraUtils;
+import com.otaliastudios.cameraview.CameraView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import kekify.io.hackteam.R;
 
 /**
@@ -14,6 +23,10 @@ import kekify.io.hackteam.R;
  */
 public class CameraFragment extends Fragment {
 
+
+    @BindView(R.id.camera)
+    CameraView cvCamera;
+    Unbinder unbinder;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -24,7 +37,38 @@ public class CameraFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        initCamera();
+    }
+
+    void initCamera(){
+        cvCamera.start();
+        cvCamera.addCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(byte[] jpeg) {
+
+                super.onPictureTaken(jpeg);
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        cvCamera.stop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+        cvCamera.destroy();
+    }
 }
